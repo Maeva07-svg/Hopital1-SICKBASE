45c8dff52a7833799e6289fe1298dc93baeb3d5a
matagneola-jpg <matagneola@gmail.com>
2026-05-11 23:16:47 +0100

    Add files via upload

---
+++ stock.h
@@ -0,0 +1,14 @@
+
+#ifndef STOCK_H
+#define STOCK_H
+
+
+
+// ================= FONCTIONS =================
+void ajouterMedicament();
+void afficherStock();
+void alerteStockFaible();
+void diminuerStock(char nom_med[]);
+int medicamentExiste(char nom_med[]);
+
+#endif
